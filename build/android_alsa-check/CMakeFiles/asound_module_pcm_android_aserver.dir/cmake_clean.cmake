c43a80c41a08473bef78b34289732abc51bb5daf
shallcheer <1722993121@qq.com>
2026-06-24 01:59:54 +0800

    Refactor code structure for improved readability and maintainability; no functional changes made.

---
+++ build/android_alsa-check/CMakeFiles/asound_module_pcm_android_aserver.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/asound_module_pcm_android_aserver.dir/module_pcm_android_aserver.c.o"
+  "CMakeFiles/asound_module_pcm_android_aserver.dir/module_pcm_android_aserver.c.o.d"
+  "libasound_module_pcm_android_aserver.dylib"
+  "libasound_module_pcm_android_aserver.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/asound_module_pcm_android_aserver.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/android_alsa-check/CMakeFiles/asound_module_pcm_android_aserver.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for asound_module_pcm_android_aserver.
+# This may be replaced when dependencies are built.
